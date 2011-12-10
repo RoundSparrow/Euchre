9e984fe163bd15604a4e94b1b0b6bfa738e9bcb0
abenjamin <abenjamin@gmail.com>
2011-12-10 17:51:40 -0600

    Initial commit

---
+++ Euchre/src/com/cynical/android/euchre/EuchreStartActivity.java
@@ -0,0 +1,31 @@
+package com.cynical.android.euchre;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class EuchreStartActivity extends Activity {
+	
+	private Button startButton;
+	private Context context;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        getResources().getDrawable(R.drawable.cards_clubs_6);
+        context = getApplicationContext();
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Toast.makeText(context, "Start Button Pressed", Toast.LENGTH_LONG).show();
+			}
+		});
+    }
+}
